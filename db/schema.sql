ebd56f03e781cfc66d8f8196b2b71c7882a4f96f
Jeffrey Allen <allenjeffreyl@gmail.com>
2017-11-09 03:01:50 -0500

    roll-back to working

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE Bill_Tracker_DB;
 
 USE Bill_Tracker_DB;
 
-CREATE TABLE user(
+CREATE TABLE User(
   user_id INT NOT NULL auto_increment,
   user_name VARCHAR(100) NOT NULL,
   login_name VARCHAR(100) NOT NULL,
@@ -13,19 +13,20 @@ CREATE TABLE user(
   PRIMARY KEY (user_id)
 );
 
-CREATE TABLE bills(
+CREATE TABLE Bills(
   bill_id INT NOT NULL auto_increment,
   user_id INT NOT NULL,
   bill_name VARCHAR(255) NOT NULL,
   bill_category VARCHAR(100),
   due_frequency VARCHAR(100) NOT NULL DEFAULT 'MONTHLY',
+  start_month DATE,
   active_status BOOLEAN NOT NULL DEFAULT TRUE,
   PRIMARY KEY (bill_id),
-  FOREIGN KEY (user_id) references user (user_id)
+  FOREIGN KEY (user_id) references User (user_id)
 );
 
 
-CREATE TABLE payments(
+CREATE TABLE Payments(
   payment_id INT NOT NULL auto_increment,
   bill_id INT NOT NULL,
   month_due DATE NOT NULL,
@@ -35,5 +36,5 @@ CREATE TABLE payments(
   amount DOUBLE(11,2),
   active_status BOOLEAN NOT NULL DEFAULT TRUE,
   PRIMARY KEY (payment_id),
-  FOREIGN KEY (bill_id) references bills (bill_id)
+  FOREIGN KEY (bill_id) references Bills (bill_id)
 );
