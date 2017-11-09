b7db75c7f0633f578362554b88e9ea938dab6dcd
Jeffrey Allen <allenjeffreyl@gmail.com>
2017-11-09 00:24:15 -0500

    try again

--- config/orm.js
+++ config/orm.js
@@ -26,7 +26,7 @@ var orm = {
   },
 
   selectAllBillsPayments: function(cb){
-    var queryString1 = "SELECT * FROM bills;";
+    var queryString1 = "SELECT * FROM bills";
     var queryString2 = "SELECT p.payment_id, p.bill_id, date_format((p.date_paid), '%m-%d-%Y') as 'date_paid_formatted', p.confirmation_code, p.amount, date_format((p.month_due), '%b-%Y') as 'month_due_formatted', p.paid_status, b.bill_name, p.active_status from payments as p inner join bills as b on p.bill_id = b.bill_id where Month(month_due) = MONTH(Current_date()) and Year(month_due) = Year(Current_date());";
     var queryString = queryString1 + ";" + queryString2;
     connection.query(queryString, function(err, result){
@@ -78,7 +78,7 @@ var orm = {
   },
 
   updateOneBill: function(table1, field1, billID, table2, field2, billID, cb) {
-    var queryString1 = "UPDATE ?? SET active_status ='0' WHERE ?? = ?;";
+    var queryString1 = "UPDATE ?? SET active_status ='0' WHERE ?? = ?";
     var queryString2 = "UPDATE ?? SET active_status ='0' WHERE ?? = ?;";
     var queryString = queryString1 + ";" + queryString2;
     connection.query(queryString,[table1, field1, billID, table2, field2, billID], function(err, result) {
